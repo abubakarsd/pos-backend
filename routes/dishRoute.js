4c209723816b11ba87cd3ec47e00de1d605c0efe
Abubakar Haruna Abdulmalik <42899173+abubakarsd@users.noreply.github.com>
2025-12-13 08:22:49 +0100

    fix(backend): add delete dish route

--- routes/dishRoute.js
+++ routes/dishRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addDish, getDishes, updateDish } = require("../controllers/dishController");
+const { addDish, getDishes, updateDish, deleteDish } = require("../controllers/dishController");
 const router = express.Router();
 const { isVerifiedUser } = require("../middlewares/tokenVerification");
 const upload = require("../middlewares/uploadMiddleware"); // Import the new middleware
@@ -9,5 +9,6 @@ router
   .post(isVerifiedUser, upload.single("image"), addDish); // Add multer middleware
 router.route("/").get(isVerifiedUser, getDishes);
 router.route("/:id").put(isVerifiedUser, updateDish);
+router.route("/:id").delete(isVerifiedUser, deleteDish);
 
 module.exports = router;
